2557e9a72c31af42d62635ca4a742eb1ebc2a9c0
semnisem <doldori102493@gmail.com>
2023-05-29 00:02:54 +0900

    initialize

--- MiniC Scanner/Java/MiniCScanner/src/TokenType.java
+++ MiniC Scanner/Java/MiniCScanner/src/TokenType.java
@@ -2,10 +2,30 @@
 public enum TokenType {
     Const, Void, Else, If, Int, While, Return,
     Eof, 
+    
     LeftBrace, RightBrace, LeftBracket, RightBracket,
     LeftParen, RightParen, Semicolon, Comma, Assign, AddAssign, SubAssign, MultAssign, DivAssign, RemAssign,
+    
     Equals, Less, LessEqual, Greater, GreaterEqual,
     Not, NotEqual, Plus, Minus, Multiply, Reminder,
     Increment, Decrement,
     Divide, And, Or, Identifier, IntLiteral,
+
+    //keywords
+    Char, Double, For, Do, Goto, Switch, Case, Break, Default,
+
+    //operator
+    Colon,
+
+    //literals
+    CharLiteral,StringLiteral,DoubleLiteral, //.123,   123. 과 같은 숏폼연산자
+
+    //comments
+    DocumentedComment,
+    SingleLineComment,
+
+    //token properties
+    FileName,
+    LineNumber,
+    ColumnNumber
 }
